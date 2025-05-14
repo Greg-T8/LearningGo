c43474b8e54b034c7e5025db0d7d7d617104103d
Greg Tate <gregt8@outlook.com>
2025-05-14 03:25:10 -0500

    Ch01: Add fetch.go to stream content from URLs provided as command-line arguments

---
+++ Books/the_go_programming_language/ch01/urls/ex-1_7/fetch.go
@@ -0,0 +1,28 @@
+// fetch.go streams the content of each URL provided as a command-line argument directly to standard output using io.Copy.
+//
+// Context: The Go Programming Language, Exercise 1.7
+// Greg Tate
+// 2025-05-14
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		// Make HTTP GET request to the URL and store result in resp struct
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		// Read the response and write it to stdout using io.Copy
+		io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+	}
+}
