468fe2ee2cbf85ea2a0b7d2c8e3ff7ebbf2fd795
Greg Tate <gregt8@outlook.com>
2025-07-06 04:33:56 -0500

    Ch01: Notes - Update web server to handle query parameters with default values for Lissajous function

--- Books/the_go_programming_language/ch01/web_server/ex_1.12/server.go
+++ Books/the_go_programming_language/ch01/web_server/ex_1.12/server.go
@@ -1,4 +1,4 @@
-// Server4: Combining the web server with the lissajous function
+// Server4: Combining the web server with the lissaous function
 
 package main
 
@@ -12,19 +12,63 @@ import (
 	"math"
 	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 func main() {
+	fmt.Println("Web server listening on 'localhost:8000'")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		cycles := ""
-		fmt.Println(r.FormValue("cycles"))
-		cycles = r.FormValue("cycles")
-		lissajous(w, cycles)
+		qp_cycles := r.FormValue("cycles")
+		if qp_cycles == "" {
+			qp_cycles = "5" // Default value if not provided
+		}
+		cycles, err := strconv.Atoi(qp_cycles)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		qp_res := r.FormValue("res")
+		if qp_res == "" {
+			qp_res = "0.001" // Default value if not provided
+		}
+		res, err := strconv.ParseFloat(qp_res, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		qp_size := r.FormValue("size")
+		if qp_size == "" {
+			qp_size = "100" // Default value if not provided
+		}
+		size, err := strconv.Atoi(qp_size)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		qp_nframes := r.FormValue("nframes")
+		if qp_nframes == "" {
+			qp_nframes = "64" // Default value if not provided
+		}
+		nframes, err := strconv.Atoi(qp_nframes)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		qp_delay := r.FormValue("delay")
+		if qp_delay == "" {
+			qp_delay = "8" // Default value if not provided
+		}
+		delay, err := strconv.Atoi(qp_delay)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		lissajous(w, cycles, res, size, nframes, delay)
 	})
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func lissajous(out io.Writer, cycles int) {
+func lissajous(out io.Writer, cycles int, res float64, size int, nframes int, delay int) {
 	// This block defines the color palette and constants for color indices
 	palette := []color.Color{
 		color.RGBA{0x00, 0x00, 0x00, 0xFF},
@@ -41,11 +85,11 @@ func lissajous(out io.Writer, cycles int) {
 		blueIndex  = 4
 	)
 	const (
-		// cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of frames in the animation
-		delay   = 8     // delay between frames in 10ms units
+	// cycles  = 5     // number of complete x oscillator revolutions
+	// res     = 0.001 // angular resolution
+	// size    = 100 // image canvas covers [-size..+size]
+	// nframes = 64 // number of frames in the animation
+	// delay   = 8  // delay between frames in 10ms units
 	)
 
 	freq := rand.Float64() * 3.0        // relative frequency of y oscillator
@@ -54,10 +98,10 @@ func lissajous(out io.Writer, cycles int) {
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res { // Note: Go does not allow automatic type conversions between int and floats, so you must do explicit conversions
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blueIndex)
+			img.SetColorIndex(size+int(x*float64(size)+0.5), size+int(y*float64(size)+0.5), blueIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
